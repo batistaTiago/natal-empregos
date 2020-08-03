c76fc9c95da9835a7a606a9b9e0da2911b16ecf8
Daniel Valente Araújo <56133316+danieelvaraujo@users.noreply.github.com>
2020-08-02 23:22:41 -0300

    Micro adição

--- resources/views/layouts/area_do_cliente.blade.php
+++ resources/views/layouts/area_do_cliente.blade.php
@@ -18,7 +18,7 @@
 </head>
 <body>
     <div class="containerlayout">
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
             <a class="navbar-brand logolayout" href="/">Natal<strong>Empregos</strong></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
