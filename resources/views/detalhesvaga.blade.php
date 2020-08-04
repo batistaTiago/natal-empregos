a84998ca7cf5a94b5c6d44c5e4e0b897954e2fa8
Daniel Valente Araújo <56133316+danieelvaraujo@users.noreply.github.com>
2020-08-03 23:11:10 -0300

    Pagina vaga criada e responsiva

--- resources/views/detalhesvaga.blade.php
+++ resources/views/detalhesvaga.blade.php
@@ -7,44 +7,54 @@
 @endsection
 
 @section('content')
-    <div class="container divdetalhes">
-        <div class="infoenvio">
-            <p>Enviado em {{ $vaga->created_at }}</p>
-        </div>
-        <div class="cabecalho">
-            <p class="titulocabeca">
-                {{ $vaga->titulo }}
-            </p>
-            <p >
-                Local de trabalho
-            </p>
-        </div>
-        <div class="detalhado">
-            <p>{{ $vaga->descricao }}</p>
-        </div>
-        <div class="divsalario">
-            <p>Remuneração: <strong>{{ isset($vaga->remuneracao) ? realMoney($vaga->remuneracao) : 'A combinar' }}</strong></p>
-        </div>
-
-        <h3>Requesitos</h3>
-        <div class="requisitos">
-            <ul>
-                <li>Habilitação A</li>
-                <li>Ter curso básico em eletrônica</li>
-                <li>Ensino médio completo</li>
-                <li>Conhecimentos em informática</li>
-            </ul>
-        </div>
-        
-        <h3>Benefícios</h3>
-        <div class="divbeneficios">
-            <ul>
-                <li>Vale transporte</li>
-                <li>Plano de saúde</li>
-                <li>Vale refeição</li>
-            </ul>
-        </div>
+<div class="heroivaga">
+    <img class='imagemheroivaga' src="{{asset("img/heroivaga.jpg")}}">
+</div>
+<div class="container divdetalhes">
+    <div class="infoenvio">
+        <p>Enviado em {{ $vaga->created_at }}</p>
     </div>
+    <div class="cabecalho">
+        <p class="titulocabeca">
+            {{-- {{ $vaga->titulo }} --}}
+            Desenvolvedor(a) Full-stack (PHP+Laravel+React)
+        </p>
+        <p >
+            Natal, RN - Aceita remoto
+        </p>
+    </div>
+    <div class="detalhado">
+        {{-- <p>{{ $vaga->descricao }}</p> --}}
+        <p>Somos uma startup voltada para a área de documentação e registro, cujo objetivo é possibilitar a adoção de novas tecnologias nos serviços notariais e de registro.</p>
+    </div>
+    <div class="divsalario">
+        {{-- <p>Remuneração: <strong>{{ isset($vaga->remuneracao) ? realMoney($vaga->remuneracao) : 'A combinar' }}</strong></p> --}}
+        <p>Remuneração: <strong>R$ 800,00</strong></p>
+    </div>
+
+    <h3>Requesitos</h3>
+    <div class="divsessao">
+        <ul>
+            <li>PHP + Laravel</li>
+            <li>Javascript + React</li>
+            <li>Versionamento GIT</li>
+        </ul>
+    </div>
+    
+    <h3>Benefícios</h3>
+    <div class="divsessao">
+        <ul>
+            <li>Contribuição nos lucros</li>
+        </ul>
+    </div>
+    <hr />
+    <br />
+
+    <h3>Contato</h3>
+    <div class="divsessao">
+        <p>Interessados mandar o CV e o repositório (Github, BitBucket) para garantistas@coding.com</p>
+    </div>
+</div>
 
 
 @endsection
