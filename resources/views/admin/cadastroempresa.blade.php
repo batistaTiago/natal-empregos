42b66d747869977b4a06a6f56f48dd9197fecf58
Lucas Laricchia <lucaslaricchia@gmail.com>
2020-08-11 01:24:12 -0300

    tela de cadastrar vaga estilizada

--- resources/views/admin/cadastroempresa.blade.php
+++ resources/views/admin/cadastroempresa.blade.php
@@ -1,44 +1,20 @@
-@extends('layouts.admin')
 
-@section('css')
-<style>
 
-</style>
-@endsection
 
-@section('content')
-    @include('flash::message')
-    <form action="{{ route('admin.empresa.cadastrar.callback') }}" method="POST" id="cadastrar-empresa-form">
-        <div class="cadastrarempresaconta">
-            @csrf
-            <label for="nome">Nome da empresa</label>
-            <input type="text" name="nome" placeholder="Nome da empresa">
-            <label for="nome_fantasia">Nome da empresa</label>
-            <input type="text" name="nome_fantasia" placeholder="Nome fantasia">
-            <label for="cnpj">CNPJ da empresa</label>
-            <input type="number" name="cnpj" placeholder="CNPJ">
-                
-            <div style="display: flex; justify-content: center; margin-top: 2vh">
-                <button type="submit" class="botao">
-                Voltar
-                </button>    
-                <button type="submit" class="botao">
-                    Enviar
-                </button>
-            </div>
+<form action="{{ route('admin.empresa.cadastrar.callback') }}" method="POST" id="cadastrar-empresa-form">
+    <div class="containercadastro">
+        @csrf
+        <label for="nome">Nome da empresa</label>
+        <input class="inputs form-control" text" name="nome" placeholder="Nome da empresa">
+        <label for="nome_fantasia">Nome da empresa</label>
+        <input class="inputs form-control" type="text" name="nome_fantasia" placeholder="Nome fantasia">
+        <label for="cnpj">CNPJ da empresa</label>
+        <input class="inputs form-control" type="number" name="cnpj" placeholder="CNPJ">
+            
+        <div style="display: flex; justify-content: center; margin-top: 2vh">
+            <button type="submit" class="botao ml-2 d-none" id="novaEmpresaFormSubmitButton">
+                Enviar
+            </button>
         </div>
-    </form>
-@endsection
-
-@section('js')
-<script>
-
-    $(() => {
-        // escrever todo o código js aqui dentro
-
-
-
-    });
-
-</script>
-@endsection
+    </div>
+</form>
