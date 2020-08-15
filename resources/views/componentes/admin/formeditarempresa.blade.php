706983af5445e9ab6470f1421224c540430dfd04
Daniel Valente Araújo <56133316+danieelvaraujo@users.noreply.github.com>
2020-08-14 23:29:12 -0300

    Form de editar das empresas (rota errada)

---
+++ resources/views/componentes/admin/formeditarempresa.blade.php
@@ -0,0 +1,18 @@
+<form action="{{ route('admin.empresa.editar.callback') }}" method="POST" id="cadastrar-empresa-form">
+    @method('PUT');
+    @csrf
+    <div class="containercadastro">
+        <label for="nome">Nome da empresa</label>
+        <input class="inputs form-control" type="text" name="nome" placeholder="Nome da empresa" value="{{ $empresa->titulo}}">
+        <label for="nome_fantasia">Nome da empresa</label>
+        <input class="inputs form-control" type="text" name="nome_fantasia" placeholder="Nome fantasia" value="{{ $empresa->nome_fantasia}}">
+        <label for="cnpj">CNPJ da empresa</label>
+        <input class="inputs form-control" type="number" name="cnpj" placeholder="CNPJ" value="{{ $empresa->cnpj }}">
+            
+        <div style="display: flex; justify-content: center; margin-top: 2vh">
+            <button type="submit" class="botao ml-2 d-none" id="novaEmpresaFormSubmitButton">
+                Enviar
+            </button>
+        </div>
+    </div>
+</form>
