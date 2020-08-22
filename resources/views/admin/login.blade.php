1521587eff476761c156b78df567630c8df210d0
tiago <tiago.sousa@arenadunas.com.br>
2020-08-22 13:04:53 -0300

    login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,26 +1,18 @@
-@extends('layouts.area_do_cliente')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NatalEmpregos</title>
+</head>
+<body>
 
-@section('css')
-<style>
+    <form method="POST" action="{{ route('admin.login.callback') }}">
+        @csrf
+        <input type="email" name="email">
+        <input type="password" name="password">
+        <button>Submit</button>
+    </form>
 
-</style>
-@endsection
-
-@section('content')
-
-
-
-@endsection
-
-@section('js')
-<script>
-
-    $(() => {
-        // escrever todo o código js aqui dentro
-
-        
-
-    });
-
-</script>
-@endsection
+</body>
+</html>
--- resources/views/componentes/admin/formeditarempresa.blade.php
+++ resources/views/componentes/admin/formeditarempresa.blade.php
@@ -3,11 +3,11 @@
     <div class="containercadastro">
         @csrf
         <label for="nome">Nome da empresa</label>
-        <input class="inputs form-control" text" name="nome" placeholder="Nome da empresa" value={{ $empresa->nome }}>
+        <input class="inputs form-control" type="text" name="nome" placeholder="Nome da empresa" value="{{ $empresa->nome }}">
         <label for="nome_fantasia">Nome da empresa</label>
-        <input class="inputs form-control" type="text" name="nome_fantasia" placeholder="Nome fantasia" value={{ $empresa->nome_fantasia }}>
+        <input class="inputs form-control" type="text" name="nome_fantasia" placeholder="Nome fantasia" value="{{ $empresa->nome_fantasia }}">
         <label for="cnpj">CNPJ da empresa</label>
-        <input class="inputs form-control" type="number" name="cnpj" placeholder="CNPJ" value={{ $empresa->cnpj }}>
+        <input class="inputs form-control" type="number" name="cnpj" placeholder="CNPJ" value="{{ $empresa->cnpj }}">
             
         <div style="display: flex; justify-content: center; margin-top: 2vh">
             <button type="submit" class="botao ml-2 d-none" id="novaEmpresaFormSubmitButton">
